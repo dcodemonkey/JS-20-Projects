6685fe0540bc9d3fe0aa512d78a10592d2bb312a
dcodemonkey <in.developer@outlook.com>
2021-12-18 13:32:18 +0530

    Adding script to get quotes fro local files

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-// let apiQuotes = [];
-
-// Show New Quote
+// Get Quotes from localQuotes using quotes.js
 // function newQuote() {
 //   // Pick a Random Quote form apiQuotes Array
-//   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-//   console.log(quote)
-// }
+//   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//   console.log(quote);
+// };
 
+// On Load
+// newQuote();
+
+let apiQuotes = [];
+
+// Show New Quote
 function newQuote() {
   // Pick a Random Quote form apiQuotes Array
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   console.log(quote);
 };
 
 // Get Quotes from API 
-// async function getQuotes() {
-//   const apiUrl = 'https://type.fit/api/quotes';
-//   try {
-//     const response = await fetch(apiUrl);
-//     apiQuotes = await response.json();
-//     newQuote();
-//   } catch(error) {
-//     // Catch Error Here
-//   }
-// }
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch(error) {
+    // Catch Error Here
+  }
+};
 
 // On Load
-// getQuotes();
+getQuotes();
 
-newQuote();
 
 
 
