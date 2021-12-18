0f8f613959e7d0fe8a2961c42e8456a3de344466
dcodemonkey <in.developer@outlook.com>
2021-12-18 13:24:15 +0530

    Fetching Data With JavaScript

---
+++ script.js
@@ -0,0 +1,34 @@
+// let apiQuotes = [];
+
+// Show New Quote
+// function newQuote() {
+//   // Pick a Random Quote form apiQuotes Array
+//   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+//   console.log(quote)
+// }
+
+function newQuote() {
+  // Pick a Random Quote form apiQuotes Array
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  console.log(quote);
+};
+
+// Get Quotes from API 
+// async function getQuotes() {
+//   const apiUrl = 'https://type.fit/api/quotes';
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     newQuote();
+//   } catch(error) {
+//     // Catch Error Here
+//   }
+// }
+
+// On Load
+// getQuotes();
+
+newQuote();
+
+
+
