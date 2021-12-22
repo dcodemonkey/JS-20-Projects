a9bc74166271f8dfee1c2d24525a8043c0bf8bb4
kunal.choudhary <kunal.choudhary@highlands.in>
2021-12-22 21:54:38 +0530

    Filling progress bar according to  music

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -87,8 +89,21 @@ function nextSong() {
 // On Load - Select First Song
 loadSong(songs[songIndex]);
 
+// Update Progress Bar and Time
+function updateProgressBar(e) {
+  if(isPlaying) {
+    const {duration, currentTime} = e.srcElement;
+    console.log(duration, currentTime);
+    // Update progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+    // console.log(`Music is progressed by ${progressPercent}`);
+    progress.style.width = `${progressPercent}%`;
+  }
+}
+
 // Event Listener
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar)
 
 
