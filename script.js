aa5064eb66de7c1e71b96d07e7da74d88bf5637e
kunal.choudhary <kunal.choudhary@highlands.in>
2021-12-23 21:12:52 +0530

    Woking on the Form Values

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const inputContainer = document.getElementById('input-container');
 const countDownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input minimum with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take value from Form Input 
+const updateCountDown = (e) => {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownTitle, countdownDate);
+}
+
+// Event Listener
+countDownForm.addEventListener('submit', updateCountDown)
