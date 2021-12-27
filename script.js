58410e5abdf4ab0d7ef3f4a5b26aae703d733c1b
Kunal Choudhary <kunal.choudhary@highlands.in>
2021-12-27 07:34:54 +0530

    JS - Favorites DOM Elements

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-function updateDOM() {
-  resultsArray.forEach((result)=>{
+function createDOMNodes(page) {
+  const currentArray = page === 'results' ? resultsArray : Object.values(favorites);
+  console.log('Current Array', page, currentArray);
+  currentArray.forEach((result)=>{
     // Card Container
     const card = document.createElement('div');
     card.classList.add('card');
@@ -63,16 +65,25 @@ function updateDOM() {
   });
 }
 
+function updateDOM(page) {
+  // Get Favorites from localStorage
+  if(localStorage.getItem('nasaFavorites')) {
+    favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
+    console.log('favourites from localStorege', favorites);
+  }
+  createDOMNodes(page);
+}
+
 // Get 10 images from NASA API
 async function getNasaPictures() {
   try {
     const response = await fetch(apiUrl);
     resultsArray = await response.json();
-    console.log(resultsArray);
-    updateDOM();
+    // console.log(resultsArray);
+    updateDOM('favorites');
   } catch (error) {
     // Catch Error Here
-    console.log(error);
+    // console.log(error);
   }
 }
 
