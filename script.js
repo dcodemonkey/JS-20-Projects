dc61393d4a3f2365da79af12e2b9828709b86154
Kunal Choudhary <kunal.choudhary@highlands.in>
2021-12-29 09:18:06 +0530

    JS - Rebuild Arrays

--- script.js
+++ script.js
@@ -95,13 +95,37 @@ function updateDOM() {
   });
 
   // Run getSavedColumns only once, Update Local Storage
+  updatedOnLoad = true;
+  updateSavedColumns();
+}
 
+// Allows arrays to reflect Drag and Drop items
+function rebuildArrays() {
+  // console.log(backlogList.children);
+  // console.log(progressList.children);
+  backlogListArray = [];
+  for(let i = 0; i < backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for(let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for(let i = 0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for(let i = 0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
 }
 
 // When Item Starts Dragging
 function drag(e) {
   draggedItem = e.target;
-  console.log('draggedItem:', draggedItem);
+  // console.log('draggedItem:', draggedItem);
 }
 
 // Coloumn Allows for Item to drop
@@ -125,6 +149,7 @@ function drop(e) {
   // Add Item to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  rebuildArrays();
 }
 
 // On Load
